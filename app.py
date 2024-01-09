cd64fad9378e01e4497d51d76d269bdf74cdb4dd
yefwal <145699390+yefwal@users.noreply.github.com>
2024-01-09 11:37:53 -0800

    synced a new csv file for recipes

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-# prompt user to add ingredients
+import csv
+import re
 
+# prompt user to add ingredients
+# store all ingredients
 total = []
 
 while True:
     prompt = input("What ingredients do you have? (Press Enter to Exit) ")
-    total.append(prompt)
-    if prompt == "":
-        for i in total:
-            print(f"{i}, ", end = "")
+    if not prompt:
         break
+    total.append(prompt.lower())  # Convert user input to lowercase for case-insensitive matching
 
-# store all ingredients
 # return all possible recipes based on ingredients provided
+with open('recipes.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)  # Skip header row
+    for row in csv_reader:
+        dish_name = row[4].replace("-"," ").title()  # Accessing the first column (index 0) which contains names
+        dish_name = re.sub(r'\d+', '', dish_name)
+        ingredients = row[1].split(',')  # Split the ingredients string into a list
+        matching_ingredients = [ingredient.lower().strip() for ingredient in ingredients if any(word in ingredient.lower().strip() for word in total)]
+        if matching_ingredients:
+            print(f'Dish: "{dish_name}" possibly available with these ingredient(s): {", ".join(matching_ingredients)}')
 
